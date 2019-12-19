34d9f23f04ff1f3aa04a26edd61f30017b5e342c
Stephanie Chiu <stephaniechiu@outlook.com>
2019-12-19 10:38:48 -0800

    Initial Commit

---
+++ SwiftyWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyWords
+//
+//  Created by Stephanie on 12/19/19.
+//  Copyright © 2019 Stephanie Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
